44c6301d7a392d5251e30f4b8400da34a8476b6a
JMCrawf <crawfjm@live.com.au>
2021-08-26 18:53:31 +0930

    adding logo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
+import logo from '../../images/logo.png';
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
@@ -24,19 +25,19 @@ const Header = () => {
   return (
     <section id="hero" className="jumbotron">
       <Container>
+        <img src={logo} className="logo" alt="Logo" />
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            {title} <span className="text-color-main">{name}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                {cta}
               </Link>
             </span>
           </p>
