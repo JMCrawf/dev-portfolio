349feccf97acde994b09800e6204e6a98f1afedf
JMCrawf <crawfjm@live.com.au>
2021-08-27 20:44:48 +0930

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1ca29bf9a0de64ec38ed.js"
+    "url": "webpack-runtime-6141128b2dfc433f8125.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "173034285a771e37ef7f9409914742e9"
+    "revision": "31aefc100f907db58963a9739c109ed6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "fa3fa5799c47192a9960bea14dd672ea"
+    "revision": "204d401ff6c403062cb92d75bd1a0921"
   },
   {
     "url": "polyfill-57fa76c8ad53bd44c5a1.js"
